dc9de9a98c3bd34cc193463b9a4163bdb698b22c
Andrew Connell <me@andrewconnell.com>
2015-04-13 15:05:14 -0400

    multiple refactoring cleanups

--- officeaddin-outlook/src/server/server.ts
+++ officeaddin-outlook/src/server/server.ts
@@ -24,14 +24,9 @@ app.use('/public', express.static(__dirname + '/../public'));
 app.use('/public/vendor', express.static(__dirname + '/../../bower_components'));
 app.use('/app', express.static(__dirname + '/../app'));
 
-// add cache control
-// app.use(express.static(__dirname +'/public', {maxAge: oneDay} ));
-
-// todo - load controllers
+// load UX and API controllers
 var controllers = new controllerFactory(app);
 controllers.init();
-
-// todo - load api
 
 // setup ssl self hosting
 var https_options = {
@@ -40,19 +35,10 @@ var https_options = {
 };
 var httpServerPort = process.env.PORT || 8443;  // use server value (for Azure) or local port
 
-// create HTTP webserver
-//var server = http.createServer(app);
-// create HTTPS webserver
-var server:any = https.createServer(https_options, app);
-
-// start webserver
-server.listen(httpServerPort);
-
-var protocol = 'http';
-if (server.cert) {
-  protocol += 's';
-}
+// create & startup HTTPS webserver
+https.createServer(https_options, app)
+     .listen(httpServerPort);
 
 console.log('+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+');
-console.log('Web Server listening at %s://[..]:%s', protocol, httpServerPort);
+console.log('Web Server listening at https://[..]:%s', httpServerPort);
 console.log('+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+');
